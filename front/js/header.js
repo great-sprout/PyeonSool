bbc271d981dd71aaa667ca4f8c4432732b4ae814
JunYoung-C <doforme524@gmail.com>
2022-06-13 10:00:42 +0900

    feat: 헤더 메뉴 아이콘 클릭 시 모달창 on/off 기능 구현

---
+++ front/js/header.js
@@ -0,0 +1,22 @@
+const UNVISIBLE = "unvisible";
+
+function addHeaderNavEvent() {
+  const headerNavBars = document.querySelector(".header__nav .fa-bars");
+  const headerNavTimes = document.querySelector(".header__nav .fa-times");
+  const listNav = document.querySelector(".list-nav");
+
+  headerNavBars.addEventListener("click", function() {
+    headerNavBars.classList.add(UNVISIBLE);
+    headerNavTimes.classList.remove(UNVISIBLE);
+    listNav.classList.remove(UNVISIBLE);
+  });
+
+  headerNavTimes.addEventListener("click", function() {
+    headerNavBars.classList.remove(UNVISIBLE);
+    headerNavTimes.classList.add(UNVISIBLE);
+    listNav.classList.add(UNVISIBLE);
+  });
+}
+
+
+addHeaderNavEvent();
