a804e776c9b15daea1890bdb6f74af3ac88b3506
JunYoung Choi <87891581+JunYoung-C@users.noreply.github.com>
2022-06-15 21:05:57 +0900

    feat: 리뷰 추천/비추천 버튼 클릭 시 색 변경 (#39)

--- front/js/detailPage.js
+++ front/js/detailPage.js
@@ -19,10 +19,51 @@ function addRecommendationSlidingEvent() {
 function addLikeClickEvent() {
   const alcoholInfoLikeButton = document.querySelector(".alcohol-info__like-button");
 
-  alcoholInfoLikeButton.addEventListener("click", function(event) {
+  alcoholInfoLikeButton.addEventListener("click", function() {
     alcoholInfoLikeButton.classList.toggle("alcohol-info__like-button--liked");
   })
 }
 
+function addReviewRecommendationEvent() {
+  document.querySelector(".reviews").addEventListener("click", function(event) {
+    const target = event.target;
+
+    if (target.classList.contains("recommendation__icon")) {
+      clearClickeState(target, ".no-recommendation__icon", ".no-recommendation__icon--clicked");
+      changeToClickState(target);
+
+    } else if (target.classList.contains("recommendation__icon--clicked")) {
+      changeToUnclickState(target);
+
+    } else if (target.classList.contains("no-recommendation__icon")) {
+      clearClickeState(target, ".recommendation__icon", ".recommendation__icon--clicked");
+      changeToClickState(target);
+      
+    } else if (target.classList.contains("no-recommendation__icon--clicked")) {
+      changeToUnclickState(target);
+    }
+  });
+
+  function changeToClickState(target) {
+    target.classList.add("unvisible");
+    target.nextElementSibling.classList.remove("unvisible");
+  }
+  
+  function changeToUnclickState(target) {
+    target.previousElementSibling.classList.remove("unvisible");
+    target.classList.add("unvisible");
+  }
+  
+  function clearClickeState(target, unclickStateSelector, clickStateSelector) {
+    const reviewPreference = target.closest(".review__preference");
+  
+    reviewPreference.querySelector(unclickStateSelector).classList.remove("unvisible");
+    reviewPreference.querySelector(clickStateSelector).classList.add("unvisible");
+  }
+}
+
+
+
 addLikeClickEvent();
+addReviewRecommendationEvent();
 addRecommendationSlidingEvent();
