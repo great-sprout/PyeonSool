04791ccbf4d1f61746ceb16b840e9b05b0436622
JunYoung Choi <87891581+JunYoung-C@users.noreply.github.com>
2022-06-19 12:59:53 +0900

    feat: 키워드 선택 창 검증 기능 추가 (#58)
    * refactor: 에러 전용 색 변수화
    
    * feat: 키워드 검증 오류 발생 시 부여할 에러 속성 지정
    
    * feat: 키워드를 3개 선택하도록 검증 기능 구현

--- front/js/signUpPage.js
+++ front/js/signUpPage.js
@@ -15,6 +15,21 @@ function addKeywordChoiceModalEvent() {
   });
 
   document.querySelector(".keyword-choice__confirm").addEventListener("click", function() {
+    
+  });
+
+  document.querySelector(".keyword-choice__confirm").addEventListener("click", function(event) {
+    const checkedKeywords = document.querySelectorAll(".keyword-choice__keyword-input:checked");
+
+    if (checkedKeywords.length !== 3) {
+      document.querySelector(".keyword-choice__choice-count").classList.add("error-keyword");
+  
+      event.preventDefault();
+      return;
+    } else {
+      document.querySelector(".keyword-choice__choice-count").classList.remove("error-keyword");
+    }
+
     document.querySelector(".modal").classList.add(UNVISIBLE);
   });
 }
