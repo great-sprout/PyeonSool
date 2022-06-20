0181112ba523b257d97206a0704306d0e9b9aaad
JunYoung-C <doforme524@gmail.com>
2022-06-20 10:47:18 +0900

    chore: spring 프로젝트 생성

---
+++ pyeonsool/src/main/java/toyproject/pyeonsool/PyeonsoolApplication.java
@@ -0,0 +1,13 @@
+package toyproject.pyeonsool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyeonsoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyeonsoolApplication.class, args);
+	}
+
+}
