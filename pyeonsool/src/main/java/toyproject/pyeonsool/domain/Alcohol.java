b78f9739947260fb8be7db8733275551e10b866c
JunYoung-C <doforme524@gmail.com>
2022-06-20 15:30:12 +0900

    feat: Alcohol 엔티티 생성

---
+++ pyeonsool/src/main/java/toyproject/pyeonsool/domain/Alcohol.java
@@ -0,0 +1,33 @@
+package toyproject.pyeonsool.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {@UniqueConstraint(name = "alcohol_name_uk", columnNames = {"name"})})
+public class Alcohol {
+    @Id
+    @GeneratedValue
+    @Column(name = "alcohol_id")
+    private Long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private AlcoholType type;
+
+    private String fileName;
+    private String name;
+    private Integer price;
+    private Float abv;
+    private Byte sugarContent;
+
+    @Enumerated(value = EnumType.STRING)
+    private Vendor vendor;
+
+    private String manufacturer;
+    private String origin;
+}
