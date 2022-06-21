0ad7d3e5c001f63ccf0df50da5b77465a4323d41
JunYoung-C <doforme524@gmail.com>
2022-06-21 18:35:42 +0900

    fix: 오타 수정

--- pyeonsool/src/main/resources/static/js/mainPage.js
+++ pyeonsool/src/main/resources/static/js/mainPage.js
@@ -1,9 +1,9 @@
-addRecommendSwitchEvent() {
-  const monthTitle = document.querySelector(".recommmend__month-title");
+function addRecommendSwitchEvent() {
+  const monthTitle = document.querySelector(".recommend__month-title");
   const recommendListMonth = document.querySelector(".recommend__list--month");
   const yoursTitle = document.querySelector(".recommend__yours-title");
   const recommendListYours = document.querySelector(".recommend__list--yours");
-  const SELECTED_TITLE = "recommmend__selected-title";
+  const SELECTED_TITLE = "recommend__selected-title";
   
   monthTitle.addEventListener("click", function(event){
     monthTitle.classList.add(SELECTED_TITLE);
