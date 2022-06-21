e8d7b0f954ef2a9323b92022b710a7abbd7d78e8
JunYoung Choi <87891581+JunYoung-C@users.noreply.github.com>
2022-06-21 17:21:02 +0900

    feat: 이달의 추천, 당신의 편술 각각의 리스트 구현 (#98)
    * chore: main.js 파일 생성
    
    * feat: 이달의 추천 슬라이드 영역 개수 표시 원 수정
    
    * docs: 당신의 편술 리스트 추가
    
    * feat: 상단 슬라이드 영역 css 조정
    
    * feat: 상단 슬라이드 제목 클릭 이벤트 구현

---
+++ pyeonsool/src/main/resources/static/js/mainPage.js
@@ -0,0 +1,21 @@
+const monthTitle = document.querySelector(".recommmend__month-title");
+const recommendListMonth = document.querySelector(".recommend__list--month");
+const yoursTitle = document.querySelector(".recommend__yours-title");
+const recommendListYours = document.querySelector(".recommend__list--yours");
+const SELECTED_TITLE = "recommmend__selected-title";
+
+monthTitle.addEventListener("click", function(event){
+  monthTitle.classList.add(SELECTED_TITLE);
+  yoursTitle.classList.remove(SELECTED_TITLE);
+
+  recommendListMonth.classList.remove("unvisible");
+  recommendListYours.classList.add("unvisible");
+});
+
+yoursTitle.addEventListener("click", function(event){
+  monthTitle.classList.remove(SELECTED_TITLE);
+  yoursTitle.classList.add(SELECTED_TITLE);
+
+  recommendListMonth.classList.add("unvisible");
+  recommendListYours.classList.remove("unvisible");
+});
