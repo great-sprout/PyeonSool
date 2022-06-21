c7a48368afb1c1b8bd1509ef8edd0e0ef1d7af2c
JunYoung-C <doforme524@gmail.com>
2022-06-21 19:00:03 +0900

    feat: 이달의 추천 리스트에 슬라이드 기능 구현

--- pyeonsool/src/main/resources/static/js/mainPage.js
+++ pyeonsool/src/main/resources/static/js/mainPage.js
@@ -22,4 +22,58 @@ function addRecommendSwitchEvent() {
   });
 }
 
+function addMonthRecommendSlideEvent() {
+  const monthRecommendUlItems = document.querySelector(".recommend__list--month .recommend__items");
+  const IMAGE_COUNT = monthRecommendUlItems.childElementCount;
+  const SLIDE_DISTANCE = 1140;
+  const SLIDE_IMAGE_COUNT = 4;
+  const leftSlideButton = document.querySelector(".recommend__list--month .slide-button--left");
+  const rightSlideButton = document.querySelector(".recommend__list--month .slide-button--right");
+  
+  const monthRecommendSlideController = new SlideController(0, monthRecommendUlItems, IMAGE_COUNT, SLIDE_IMAGE_COUNT,  SLIDE_DISTANCE, leftSlideButton, rightSlideButton);
+  monthRecommendSlideController.controlButtons();
+
+  leftSlideButton.addEventListener("click", function(event) {
+    monthRecommendSlideController.controlButtonSlide(event.target);
+
+    changeSelectedCircle(IMAGE_COUNT, SLIDE_IMAGE_COUNT, monthRecommendSlideController.index);
+  });
+  
+  rightSlideButton.addEventListener("click", function(event) {
+    monthRecommendSlideController.controlButtonSlide(event.target);
+
+    changeSelectedCircle(IMAGE_COUNT, SLIDE_IMAGE_COUNT, monthRecommendSlideController.index);
+  });
+
+  createSlideCircle(IMAGE_COUNT, SLIDE_IMAGE_COUNT);
+}
+
+function createSlideCircle(IMAGE_COUNT, SLIDE_IMAGE_COUNT) {
+  const circleCount = IMAGE_COUNT / SLIDE_IMAGE_COUNT;
+  for (let i = 0; i < circleCount; i++) {
+    const div = document.createElement("div");
+    div.className = "recommend__count-circle";
+
+    if (i == 0) {
+      div.classList.add("recommend__count-circle--selected");
+    }
+
+    document.querySelector(".recommend__list--month .recommend__count-circles").appendChild(div);
+  }
+}
+
+function changeSelectedCircle(IMAGE_COUNT, SLIDE_IMAGE_COUNT, index) {
+  const countCircle = document.querySelectorAll(".recommend__count-circle");
+  const circleCount = IMAGE_COUNT / SLIDE_IMAGE_COUNT;
+
+  for (let i = 0; i < circleCount; i++) {
+    if (i == index) {
+      countCircle[i].classList.add("recommend__count-circle--selected");
+    } else {
+      countCircle[i].classList.remove("recommend__count-circle--selected");
+    }
+  }
+}
+
 addRecommendSwitchEvent();
+addMonthRecommendSlideEvent();
