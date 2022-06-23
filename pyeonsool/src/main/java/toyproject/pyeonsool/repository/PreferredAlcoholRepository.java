babf677112a031ed58d51e528fa572af1c4eb4b4
JunYoung-C <doforme524@gmail.com>
2022-06-23 20:11:04 +0900

    feat: 회원과 술로 선호하는 알코올이 존재하는지 확인하는 메서드 구현

--- pyeonsool/src/main/java/toyproject/pyeonsool/repository/PreferredAlcoholRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/repository/PreferredAlcoholRepository.java
@@ -1,7 +1,11 @@
 package toyproject.pyeonsool.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import toyproject.pyeonsool.domain.Alcohol;
+import toyproject.pyeonsool.domain.Member;
 import toyproject.pyeonsool.domain.PreferredAlcohol;
 
 public interface PreferredAlcoholRepository  extends JpaRepository<PreferredAlcohol, Long> {
+
+    boolean existsByMemberAndAlcohol(Member member, Alcohol alcohol);
 }
