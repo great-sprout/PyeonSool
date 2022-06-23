5324d3808054eccb9da5f0572d64778cb43eba87
wyu950 <dnjsdusdnr@naver.com>
2022-06-23 09:50:35 +0900

    feat: 초기 데이터 값 추가 키워드 (#118)

--- pyeonsool/src/main/java/toyproject/pyeonsool/initDB.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/initDB.java
@@ -39,8 +39,48 @@ public class initDB {
             Keyword[] keywords = new Keyword[21];
             keywords[0] = new Keyword("sweet");
             keywords[1] = new Keyword("clear");
+            keywords[2] = new Keyword("cool");
+            keywords[3] = new Keyword("heavy");
+            keywords[4] = new Keyword("light");
+            keywords[5] = new Keyword("nutty");
+            keywords[6] = new Keyword("fresh");
+            keywords[7] = new Keyword("flower");
+            keywords[8] = new Keyword("bitter");
+            keywords[9] = new Keyword("unique");
+            keywords[10] = new Keyword("strong");
+            keywords[11] = new Keyword("middle");
+            keywords[12] = new Keyword("mild");
+            keywords[13] = new Keyword("white");
+            keywords[14] = new Keyword("red");
+            keywords[15] = new Keyword("astringent");
+            keywords[16] = new Keyword("fruit");
+            keywords[17] = new Keyword("nonAlcoholic");
+            keywords[18] = new Keyword("highAcidity");
+            keywords[19] = new Keyword("middleAcidity");
+            keywords[20] = new Keyword("lowAcidity");
+
             em.persist(keywords[0]);
             em.persist(keywords[1]);
+            em.persist(keywords[2]);
+            em.persist(keywords[3]);
+            em.persist(keywords[4]);
+            em.persist(keywords[5]);
+            em.persist(keywords[6]);
+            em.persist(keywords[7]);
+            em.persist(keywords[8]);
+            em.persist(keywords[9]);
+            em.persist(keywords[10]);
+            em.persist(keywords[11]);
+            em.persist(keywords[12]);
+            em.persist(keywords[13]);
+            em.persist(keywords[14]);
+            em.persist(keywords[15]);
+            em.persist(keywords[16]);
+            em.persist(keywords[17]);
+            em.persist(keywords[18]);
+            em.persist(keywords[19]);
+            em.persist(keywords[20]);
+
 
             ArrayList<Alcohol> alcohols = new ArrayList<>();
             alcohols.add(new Alcohol(WINE, "louis-jadot.png",
