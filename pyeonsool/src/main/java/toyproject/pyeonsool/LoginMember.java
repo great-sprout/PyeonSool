789242f8fa6fc6ff01faf853431332fe19dba7cd
JunYoung-C <doforme524@gmail.com>
2022-06-24 17:22:39 +0900

    feat: 로그인 처리 세션 적용

--- pyeonsool/src/main/java/toyproject/pyeonsool/LoginMember.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/LoginMember.java
@@ -2,9 +2,11 @@ package toyproject.pyeonsool;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class LoginMember {
     private Long id;
     private String nickname;
--- pyeonsool/src/main/java/toyproject/pyeonsool/web/MemberController.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/web/MemberController.java
@@ -1,12 +1,23 @@
 package toyproject.pyeonsool.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import toyproject.pyeonsool.LoginMember;
+import toyproject.pyeonsool.SessionConst;
+import toyproject.pyeonsool.domain.Member;
+import toyproject.pyeonsool.service.MemberService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/{memberId}")
     public String getMyPage(@PathVariable Long memberId) {
@@ -19,7 +30,12 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public String login(LoginForm loginForm, @RequestParam(defaultValue = "/") String redirectURL) {
+    public String login(LoginForm loginForm, @RequestParam(defaultValue = "/") String redirectURL,
+                        HttpServletRequest request) {
+        LoginMember loginMember = memberService.findLoginMember(loginForm.getUserId(), loginForm.getPassword());
+
+        HttpSession session = request.getSession(true);
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
         return "redirect:/" + redirectURL;
     }
