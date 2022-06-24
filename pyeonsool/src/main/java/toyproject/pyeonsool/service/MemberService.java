ad7f89d9762dbae7a26f1e6e4400e86cb02c54f5
JunYoung-C <doforme524@gmail.com>
2022-06-24 16:49:25 +0900

    feat: 로그인한 회원을 조회하는 기능 구현

---
+++ pyeonsool/src/main/java/toyproject/pyeonsool/service/MemberService.java
@@ -0,0 +1,29 @@
+package toyproject.pyeonsool.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import toyproject.pyeonsool.domain.Member;
+import toyproject.pyeonsool.repository.MemberRepository;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member findLoginMember(String userId, String password) {
+        Member findMember = memberRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 아이디입니다."));
+
+        if (!findMember.getPassword().equals(password)) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+
+        // TODO 예외처리 필요
+
+        return findMember;
+    }
+}
