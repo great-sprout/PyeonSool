73c5989f3cca364e69546f6939f09e0d98f0d4a9
JunYoung-C <doforme524@gmail.com>
2022-06-24 16:49:40 +0900

    test: 로그인한 회원을 조회하는 기능 테스트

---
+++ pyeonsool/src/test/java/toyproject/pyeonsool/service/MemberServiceTest.java
@@ -0,0 +1,68 @@
+package toyproject.pyeonsool.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import toyproject.pyeonsool.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    EntityManager em;
+
+    @Nested
+    class findLoginMemberTest {
+        @Test
+        void success() {
+            //given
+            String userId = "userId";
+            String password = "password";
+            Member member = new Member("nickname", userId, password, "01012345678");
+
+            em.persist(member);
+            //when
+            Member loginMember = memberService.findLoginMember(userId, password);
+
+            //then
+            assertThat(loginMember.getUserId()).isEqualTo(userId);
+            assertThat(loginMember.getPassword()).isEqualTo(password);
+        }
+
+        @Test
+        void fail_id() {
+            //given
+            //when
+            //then
+            assertThrowsExactly(IllegalArgumentException.class,
+                    () -> memberService.findLoginMember("userId", "password"),
+                    "잘못된 아이디입니다.");
+        }
+
+        @Test
+        void fail_password() {
+            //given
+            String userId = "userId";
+            String password = "password";
+            Member member = new Member("nickname", userId, password, "01012345678");
+
+            //when
+            //then
+            assertThrowsExactly(IllegalArgumentException.class,
+                    () -> memberService.findLoginMember("userId", "wrongPassword"),
+                    "잘못된 비밀번호입니다.");
+        }
+    }
+}
