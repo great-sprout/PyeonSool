238291cd013aa6bf53d4849c6feb2a9e37471e76
JunYoung-C <doforme524@gmail.com>
2022-06-24 12:42:17 +0900

    feat: 상세페이지 조회 메소드에 파라미터와 모델에 추가하는 로직 추가

--- pyeonsool/src/main/java/toyproject/pyeonsool/web/AlcoholController.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/web/AlcoholController.java
@@ -1,16 +1,29 @@
 package toyproject.pyeonsool.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import toyproject.pyeonsool.service.AlcoholDetailsDto;
+import toyproject.pyeonsool.service.AlcoholService;
 
 @Controller
 @RequestMapping("/alcohols")
+@RequiredArgsConstructor
 public class AlcoholController {
 
+    private final AlcoholService alcoholService;
+
     @GetMapping("/{alcoholId}")
-    public String getDetailPage(@PathVariable Long alcoholId) {
+    public String getDetailPage(@PathVariable Long alcoholId,
+                                @SessionAttribute(name = "memberId", required = false) Long memberId,
+                                Model model) {
+        AlcoholDetailsDto alcoholDetails = alcoholService.getAlcoholDetails(alcoholId, memberId);
+        model.addAttribute("alcoholDetails", alcoholDetails);
+
         return "detailPage";
     }
 }
