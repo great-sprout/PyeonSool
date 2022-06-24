cc481881752e8afbce8ca37bbd85304a7f44872f
JunYoung-C <doforme524@gmail.com>
2022-06-24 16:18:44 +0900

    feat: 아이디에 해당되는 회원을 조회하는 기능 추가

--- pyeonsool/src/main/java/toyproject/pyeonsool/repository/MemberRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import toyproject.pyeonsool.domain.Member;
 
 public interface MemberRepository  extends JpaRepository<Member, Long> {
+
+    public Member findByUserId(String userId);
 }
