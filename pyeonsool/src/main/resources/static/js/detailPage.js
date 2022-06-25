406a0e326f0833bf0ce9a336058ce9e3e3c6e28f
JunYoung Choi <87891581+JunYoung-C@users.noreply.github.com>
2022-06-26 00:50:50 +0900

    feat: 술 좋아요, 취소 ajax 구현 (#134)
    * feat: 술 좋아요, 좋아요 취소 ajax 구현
    
    * refactor: 술 좋아요, 좋아요 취소 ajax 리팩터링

--- pyeonsool/src/main/resources/static/js/detailPage.js
+++ pyeonsool/src/main/resources/static/js/detailPage.js
@@ -4,14 +4,14 @@ function addRecommendationSlidingEvent() {
   const SLIDE_DISTANCE = 1190;
   const leftSlideButton = document.querySelector(".slide-button--left");
   const rightSlideButton = document.querySelector(".slide-button--right");
-  
+
   const recommendationSlideController = new SlideController(10, alcoholRecommendationItems, IMAGE_COUNT, 5,  SLIDE_DISTANCE, leftSlideButton, rightSlideButton);
   recommendationSlideController.controlButtons();
-  
+
   leftSlideButton.addEventListener("click", function(event) {
     recommendationSlideController.controlButtonSlide(event.target);
   });
-  
+
   rightSlideButton.addEventListener("click", function(event) {
     recommendationSlideController.controlButtonSlide(event.target);
   });
@@ -21,8 +21,61 @@ function addLikeClickEvent() {
   const alcoholInfoLikeButton = document.querySelector(".alcohol-info__like-button");
 
   alcoholInfoLikeButton.addEventListener("click", function() {
-    alcoholInfoLikeButton.classList.toggle("alcohol-info__like-button--liked");
+    const LIKE_STATUS_CLASS=  "alcohol-info__like-button--liked";
+    if (alcoholInfoLikeButton.classList.contains(LIKE_STATUS_CLASS)) {
+      // dislike ajax
+      return dislikeAlcoholAjax(LIKE_STATUS_CLASS);
+    } else {
+      // like ajax
+      return likeAlcoholAjax(LIKE_STATUS_CLASS);
+    }
   })
+
+  function dislikeAlcoholAjax(LIKE_STATUS_CLASS) {
+    const request = new XMLHttpRequest();
+
+    if (!request) {
+      alert("XMLHTTP 인스턴스 생성 불가");
+      return false;
+    }
+
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          alcoholInfoLikeButton.classList.remove(LIKE_STATUS_CLASS);
+        } else {
+          alert(request.response.message);
+        }
+      }
+    }
+
+    request.open("post", window.location.pathname + "/dislike");
+    request.responseType = "json";
+    request.send();
+  }
+
+  function likeAlcoholAjax(LIKE_STATUS_CLASS) {
+    const request = new XMLHttpRequest();
+
+    if (!request) {
+      alert("XMLHTTP 인스턴스 생성 불가");
+      return false;
+    }
+
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          alcoholInfoLikeButton.classList.add(LIKE_STATUS_CLASS);
+        } else {
+          alert(request.response.message);
+        }
+      }
+    }
+
+    request.open("post", window.location.pathname + "/like");
+    request.responseType = "json";
+    request.send();
+  }
 }
 
 function addReviewRecommendationEvent() {
@@ -39,7 +92,7 @@ function addReviewRecommendationEvent() {
     } else if (target.classList.contains("no-recommendation__icon")) {
       clearClickeState(target, ".recommendation__icon", ".recommendation__icon--clicked");
       changeToClickState(target);
-      
+
     } else if (target.classList.contains("no-recommendation__icon--clicked")) {
       changeToUnclickState(target);
     }
@@ -49,15 +102,15 @@ function addReviewRecommendationEvent() {
     target.classList.add("unvisible");
     target.nextElementSibling.classList.remove("unvisible");
   }
-  
+
   function changeToUnclickState(target) {
     target.previousElementSibling.classList.remove("unvisible");
     target.classList.add("unvisible");
   }
-  
+
   function clearClickeState(target, unclickStateSelector, clickStateSelector) {
     const reviewPreference = target.closest(".review__preference");
-  
+
     reviewPreference.querySelector(unclickStateSelector).classList.remove("unvisible");
     reviewPreference.querySelector(clickStateSelector).classList.add("unvisible");
   }
@@ -69,14 +122,14 @@ function addReviewEditStateEvent() {
     if (review === null) {
       return;
     }
-    
+
     const reviewContent = review.querySelector(".review__content");
     const reviewTextarea = review.querySelector(".review__textarea");
     const reviewButtons = review.querySelector(".review__buttons");
     const reviewEditor = review.querySelector(".review__editor");
     const fixedStars = review.querySelector(".review__fixed-stars");
     const revieStars = review.querySelector(".review__stars");
-    
+
     if (event.target.classList.contains("review__edit-status")) {
       // 수정 가능 상태
       reviewTextarea.textContent = reviewContent.innerText;
@@ -87,7 +140,7 @@ function addReviewEditStateEvent() {
       reviewTextarea.classList.remove("unvisible");
       reviewButtons.classList.add("unvisible");
       reviewEditor.classList.remove("unvisible");
-  
+
     } else if (event.target.classList.contains("review__cancel")) {
       // 수정 불가능 상태
       fixedStars.classList.remove("unvisible");
@@ -97,7 +150,7 @@ function addReviewEditStateEvent() {
       reviewButtons.classList.remove("unvisible");
       reviewEditor.classList.add("unvisible");
     }
-  
+
   });
 }
 
