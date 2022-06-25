a3c9e85510ab8a38286596f38194ca1fe783261a
JunYoung-C <doforme524@gmail.com>
2022-06-25 12:16:17 +0900

    test: 특정 술의 판매처를 조회하는 메소드 테스트

---
+++ pyeonsool/src/test/java/toyproject/pyeonsool/repository/VendorRepositoryTest.java
@@ -0,0 +1,45 @@
+package toyproject.pyeonsool.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import toyproject.pyeonsool.AppConfig;
+import toyproject.pyeonsool.domain.*;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static toyproject.pyeonsool.domain.AlcoholType.BEER;
+import static toyproject.pyeonsool.domain.VendorName.*;
+
+@DataJpaTest
+@Import(AppConfig.class)
+class VendorRepositoryTest {
+
+    @Autowired
+    VendorRepository vendorRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void getAlcoholVendors() {
+        //given
+        Alcohol alcohol = new Alcohol(BEER, "san-miguel.png", "산미구엘 페일필젠", 3000,
+                5f, null, null, "산미구엘 브루어리", "필리핀");
+        em.persist(alcohol);
+
+        em.persist(new Vendor(alcohol, CU));
+        em.persist(new Vendor(alcohol, GS25));
+        em.persist(new Vendor(alcohol, SEVEN_ELEVEN));
+
+        //then
+        List<String> alcoholVendors = vendorRepository.getAlcoholVendors(alcohol.getId());
+
+        //then
+        Assertions.assertThat(alcoholVendors).containsOnly(CU.name(), GS25.name(), SEVEN_ELEVEN.name());
+    }
+}
