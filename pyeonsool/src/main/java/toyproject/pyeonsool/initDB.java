56bd80bf19db95a82561f75fea16a07a6c108ff1
sjihwan <rocrie12@naver.com>
2022-06-25 16:15:50 +0900

    feat: 와인 초기 데이터 값 추가

--- pyeonsool/src/main/java/toyproject/pyeonsool/initDB.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/initDB.java
@@ -59,61 +59,17 @@ public class initDB {
             persistBeerKeywords(keywords, beers);
             persistBeerVendors(beers);
 
+            //와인 데이터 넣기
+            ArrayList<Alcohol> wines = new ArrayList<>();
+            setWines(wines);
+            for (Alcohol alcohol : wines) {
+                em.persist(alcohol);
+            }
+            persistWineKeywords(keywords, wines);
+            persistWineVendors(wines);
+
             em.flush();
             em.clear();
-        }
-
-        private void setBeers(ArrayList<Alcohol> beers) {
-            beers.add(new Alcohol(BEER, "san-miguel.png", "산미구엘 페일필젠", 3000,
-                    5f, null, null, "산미구엘 브루어리", "필리핀"));
-            beers.add(new Alcohol(BEER, "tiger.jpg", "타이거 아시안 라거", 2500,
-                    5f, null, null, "아시아 퍼시픽 브루어리", "싱가포르"));
-            beers.add(new Alcohol(BEER, "budweiser.png", "버드와이저", 2500,
-                    5f, null, null, "앤하이저부시", "미국"));
-            beers.add(new Alcohol(BEER, "gomyo.png", "곰표 밀맥주", 2500,
-                    4.5f, null, null, "세븐브로이맥주", "한국"));
-            beers.add(new Alcohol(BEER, "hite-extra-cold.png", "하이트 엑스트라 콜드", 2800,
-                    4.5f, null, null, "하이트진로", "한국"));
-            beers.add(new Alcohol(BEER, "kozel-premium-lager.png", "코젤 프리미엄 라거", 2500,
-                    4.6f, null, null, "필젠스키 프레즈드로이", "체코"));
-            beers.add(new Alcohol(BEER, "somersby.png", "서머스비(사과)", 4200,
-                    4.5f, null, null, "칼스버그 서플라이", "덴마크"));
-            beers.add(new Alcohol(BEER, "stella-artois.png", "스텔라 아르투아", 3000,
-                    5f, null, null, "스텔라 아르투아 브루어리", "벨기에"));
-            beers.add(new Alcohol(BEER, "guinness-draught.png", "기네스 드래프트", 3000,
-                    4.2f, null, null, "디아지오 아일랜드", "아일랜드"));
-        }
-
-        private void persistSojuVendors(ArrayList<Alcohol> sojus) {
-            em.persist(new Vendor(sojus.get(0), VendorName.valueOf("CU")));
-            em.persist(new Vendor(sojus.get(0), VendorName.valueOf("GS25")));
-            em.persist(new Vendor(sojus.get(0), VendorName.valueOf("SEVEN_ELEVEN")));
-
-            em.persist(new Vendor(sojus.get(1), VendorName.valueOf("CU")));
-            em.persist(new Vendor(sojus.get(1), VendorName.valueOf("GS25")));
-            em.persist(new Vendor(sojus.get(1), VendorName.valueOf("SEVEN_ELEVEN")));
-
-            em.persist(new Vendor(sojus.get(2), VendorName.valueOf("CU")));
-            em.persist(new Vendor(sojus.get(2), VendorName.valueOf("GS25")));
-            em.persist(new Vendor(sojus.get(2), VendorName.valueOf("SEVEN_ELEVEN")));
-
-            em.persist(new Vendor(sojus.get(3), VendorName.valueOf("CU")));
-            em.persist(new Vendor(sojus.get(3), VendorName.valueOf("GS25")));
-            em.persist(new Vendor(sojus.get(3), VendorName.valueOf("SEVEN_ELEVEN")));
-
-            em.persist(new Vendor(sojus.get(4), VendorName.valueOf("CU")));
-            em.persist(new Vendor(sojus.get(4), VendorName.valueOf("GS25")));
-            em.persist(new Vendor(sojus.get(4), VendorName.valueOf("SEVEN_ELEVEN")));
-
-            em.persist(new Vendor(sojus.get(5), VendorName.valueOf("CU")));
-            em.persist(new Vendor(sojus.get(5), VendorName.valueOf("GS25")));
-
-            em.persist(new Vendor(sojus.get(6), VendorName.valueOf("CU")));
-
-            em.persist(new Vendor(sojus.get(7), VendorName.valueOf("GS25")));
-
-            em.persist(new Vendor(sojus.get(8), VendorName.valueOf("CU")));
-            em.persist(new Vendor(sojus.get(8), VendorName.valueOf("GS25")));
         }
 
         private void setSojuList(ArrayList<Alcohol> sojus) {
@@ -169,11 +125,65 @@ public class initDB {
 
             em.persist(new AlcoholKeyword(keywords[1],sojus.get(7)));
             em.persist(new AlcoholKeyword(keywords[10],sojus.get(7)));
-            
+
             em.persist(new AlcoholKeyword(keywords[1],sojus.get(8)));
             em.persist(new AlcoholKeyword(keywords[5],sojus.get(8)));
             em.persist(new AlcoholKeyword(keywords[10],sojus.get(8)));
         }
+
+        private void persistSojuVendors(ArrayList<Alcohol> sojus) {
+            em.persist(new Vendor(sojus.get(0), VendorName.valueOf("CU")));
+            em.persist(new Vendor(sojus.get(0), VendorName.valueOf("GS25")));
+            em.persist(new Vendor(sojus.get(0), VendorName.valueOf("SEVEN_ELEVEN")));
+
+            em.persist(new Vendor(sojus.get(1), VendorName.valueOf("CU")));
+            em.persist(new Vendor(sojus.get(1), VendorName.valueOf("GS25")));
+            em.persist(new Vendor(sojus.get(1), VendorName.valueOf("SEVEN_ELEVEN")));
+
+            em.persist(new Vendor(sojus.get(2), VendorName.valueOf("CU")));
+            em.persist(new Vendor(sojus.get(2), VendorName.valueOf("GS25")));
+            em.persist(new Vendor(sojus.get(2), VendorName.valueOf("SEVEN_ELEVEN")));
+
+            em.persist(new Vendor(sojus.get(3), VendorName.valueOf("CU")));
+            em.persist(new Vendor(sojus.get(3), VendorName.valueOf("GS25")));
+            em.persist(new Vendor(sojus.get(3), VendorName.valueOf("SEVEN_ELEVEN")));
+
+            em.persist(new Vendor(sojus.get(4), VendorName.valueOf("CU")));
+            em.persist(new Vendor(sojus.get(4), VendorName.valueOf("GS25")));
+            em.persist(new Vendor(sojus.get(4), VendorName.valueOf("SEVEN_ELEVEN")));
+
+            em.persist(new Vendor(sojus.get(5), VendorName.valueOf("CU")));
+            em.persist(new Vendor(sojus.get(5), VendorName.valueOf("GS25")));
+
+            em.persist(new Vendor(sojus.get(6), VendorName.valueOf("CU")));
+
+            em.persist(new Vendor(sojus.get(7), VendorName.valueOf("GS25")));
+
+            em.persist(new Vendor(sojus.get(8), VendorName.valueOf("CU")));
+            em.persist(new Vendor(sojus.get(8), VendorName.valueOf("GS25")));
+        }
+
+        private void setBeers(ArrayList<Alcohol> beers) {
+            beers.add(new Alcohol(BEER, "san-miguel.png", "산미구엘 페일필젠", 3000,
+                    5f, null, null, "산미구엘 브루어리", "필리핀"));
+            beers.add(new Alcohol(BEER, "tiger.jpg", "타이거 아시안 라거", 2500,
+                    5f, null, null, "아시아 퍼시픽 브루어리", "싱가포르"));
+            beers.add(new Alcohol(BEER, "budweiser.png", "버드와이저", 2500,
+                    5f, null, null, "앤하이저부시", "미국"));
+            beers.add(new Alcohol(BEER, "gomyo.png", "곰표 밀맥주", 2500,
+                    4.5f, null, null, "세븐브로이맥주", "한국"));
+            beers.add(new Alcohol(BEER, "hite-extra-cold.png", "하이트 엑스트라 콜드", 2800,
+                    4.5f, null, null, "하이트진로", "한국"));
+            beers.add(new Alcohol(BEER, "kozel-premium-lager.png", "코젤 프리미엄 라거", 2500,
+                    4.6f, null, null, "필젠스키 프레즈드로이", "체코"));
+            beers.add(new Alcohol(BEER, "somersby.png", "서머스비(사과)", 4200,
+                    4.5f, null, null, "칼스버그 서플라이", "덴마크"));
+            beers.add(new Alcohol(BEER, "stella-artois.png", "스텔라 아르투아", 3000,
+                    5f, null, null, "스텔라 아르투아 브루어리", "벨기에"));
+            beers.add(new Alcohol(BEER, "guinness-draught.png", "기네스 드래프트", 3000,
+                    4.2f, null, null, "디아지오 아일랜드", "아일랜드"));
+        }
+
         private void persistBeerKeywords(Keyword[] keywords, ArrayList<Alcohol> beers) {
             em.persist(new AlcoholKeyword(keywords[17], beers.get(0)));
             em.persist(new AlcoholKeyword(keywords[5], beers.get(0)));
@@ -218,6 +228,100 @@ public class initDB {
                 em.persist(new Vendor(beer, VendorName.GS25));
                 em.persist(new Vendor(beer, VendorName.SEVEN_ELEVEN));
             }
+        }
+
+        private void setWines(ArrayList<Alcohol> wines) {
+            wines.add(new Alcohol(WINE, "louis-jadot.png", "루이자도 부르고뉴 샤르도네", 35000,
+                    13f, (byte) 1, (byte) 3, "루이 자도", "프랑스 부르고뉴"));
+            wines.add(new Alcohol(WINE, "michele-le-orme.png", "미켈레 레오르메", 40000,
+                    13f, (byte) 0, (byte) 5, "프레스코발디", "이탈리아"));
+            wines.add(new Alcohol(WINE, "frescobaldi-remole-bianco.png", "프레스코발디 레몰레 비앙코", 29000,
+                    12.5f, (byte) 0, (byte) 2, "미켈레 끼아를로", "이탈리아"));
+            wines.add(new Alcohol(WINE, "tignanello-grappa.png", "티냐넬로그라파", 190000,
+                    42f, (byte) 1, (byte) 5, "마르케시 안토리니", "이탈리아"));
+            wines.add(new Alcohol(WINE, "della-trappola.png", "리카솔리 안티코 페우도 델라 트라폴라 토스카나 IGT", 50000,
+                    14f, (byte) 1, (byte) 5, "리카솔리", "이탈리아"));
+            wines.add(new Alcohol(WINE, "gancia-moscato-dasti.png", "간치아 모스카토 다스티 DO", 20680,
+                    7.5f, (byte) 3, (byte) 2, "간치아", "이탈리아"));
+            wines.add(new Alcohol(WINE, "nederburg-duet-shiraz-pinotage.png", "니더버그듀엣 쉬라즈 & 피노타지", 16280,
+                    14.5f, (byte) 1, (byte) 4, "니더버그", "남아프리카공화국"));
+            wines.add(new Alcohol(WINE, "canti-moscato-dasti.png", "칸티 모스카토 다스티", 22000,
+                    7f, (byte) 4, (byte) 2, "칸티", "이탈리아"));
+            wines.add(new Alcohol(WINE, "sierra-cantabria-rose.png", "시에라 칸따브리아 로제", 30000,
+                    13.5f, (byte) 1, (byte) 3, "시에라", "스페인"));
+        }
+
+        private void persistWineKeywords(Keyword[] keywords, ArrayList<Alcohol> wines) {
+            em.persist(new AlcoholKeyword(keywords[7], wines.get(0))); //꽃
+            em.persist(new AlcoholKeyword(keywords[16], wines.get(0))); //과일
+            em.persist(new AlcoholKeyword(keywords[11], wines.get(0))); //도수 중간
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(0))); //낮은 산도
+
+            em.persist(new AlcoholKeyword(keywords[16], wines.get(1))); //과일
+            em.persist(new AlcoholKeyword(keywords[14], wines.get(1))); //레드
+            em.persist(new AlcoholKeyword(keywords[3], wines.get(1))); //무거움
+            em.persist(new AlcoholKeyword(keywords[11], wines.get(1))); //도수 중간
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(1))); //낮은 산도
+
+            em.persist(new AlcoholKeyword(keywords[7], wines.get(2))); //꽃
+            em.persist(new AlcoholKeyword(keywords[16], wines.get(2))); //과일
+            em.persist(new AlcoholKeyword(keywords[13], wines.get(2))); //화이트
+            em.persist(new AlcoholKeyword(keywords[11], wines.get(2))); //도수 중간
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(2))); //중간 산도
+
+            em.persist(new AlcoholKeyword(keywords[3], wines.get(3))); //무거움
+            em.persist(new AlcoholKeyword(keywords[10], wines.get(3))); //도수 독함
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(3))); //낮은 산도
+
+            em.persist(new AlcoholKeyword(keywords[16], wines.get(4))); //과일향
+            em.persist(new AlcoholKeyword(keywords[3], wines.get(4))); //무거움
+            em.persist(new AlcoholKeyword(keywords[15], wines.get(4))); //떫음
+            em.persist(new AlcoholKeyword(keywords[11], wines.get(4))); //도수 중간
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(4))); //낮은 산도
+
+            em.persist(new AlcoholKeyword(keywords[7], wines.get(5))); //꽃
+            em.persist(new AlcoholKeyword(keywords[16], wines.get(5))); //과일
+            em.persist(new AlcoholKeyword(keywords[11], wines.get(5))); //도수 중간
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(5))); //낮은 산도
+
+            em.persist(new AlcoholKeyword(keywords[16], wines.get(6))); //과일
+            em.persist(new AlcoholKeyword(keywords[3], wines.get(6))); //무거움
+            em.persist(new AlcoholKeyword(keywords[11], wines.get(6))); //도수 중간
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(6))); //낮은 산도
+
+            em.persist(new AlcoholKeyword(keywords[7], wines.get(7))); //꽃
+            em.persist(new AlcoholKeyword(keywords[0], wines.get(7))); //달콤함
+            em.persist(new AlcoholKeyword(keywords[13], wines.get(7))); //화이트
+            em.persist(new AlcoholKeyword(keywords[12], wines.get(7))); //도수 순함
+            em.persist(new AlcoholKeyword(keywords[20], wines.get(7))); //낮은 산도
+
+            em.persist(new AlcoholKeyword(keywords[16], wines.get(8))); //과일
+            em.persist(new AlcoholKeyword(keywords[11], wines.get(8))); //도수 중간
+            em.persist(new AlcoholKeyword(keywords[19], wines.get(8))); //중간 산도
+        }
+
+        private void persistWineVendors(ArrayList<Alcohol> wines) {
+            em.persist(new Vendor(wines.get(0), VendorName.valueOf("GS25")));
+
+            em.persist(new Vendor(wines.get(1), VendorName.valueOf("CU")));
+
+            em.persist(new Vendor(wines.get(2), VendorName.valueOf("CU")));
+            em.persist(new Vendor(wines.get(2), VendorName.valueOf("SEVEN_ELEVEN")));
+
+            em.persist(new Vendor(wines.get(3), VendorName.valueOf("CU")));
+
+            em.persist(new Vendor(wines.get(4), VendorName.valueOf("CU")));
+
+            em.persist(new Vendor(wines.get(5), VendorName.valueOf("CU")));
+
+            em.persist(new Vendor(wines.get(6), VendorName.valueOf("CU")));
+
+            em.persist(new Vendor(wines.get(7), VendorName.valueOf("CU")));
+            em.persist(new Vendor(wines.get(7), VendorName.valueOf("GS25")));
+
+            em.persist(new Vendor(wines.get(8), VendorName.valueOf("CU")));
+            em.persist(new Vendor(wines.get(8), VendorName.valueOf("GS25")));
+            em.persist(new Vendor(wines.get(8), VendorName.valueOf("SEVEN_ELEVEN")));
         }
 
         private void setKeywords(Keyword[] keywords) {
