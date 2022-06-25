4e81ad93fb7bfb60bc0483c35481aa7398392075
JunYoung Choi <87891581+JunYoung-C@users.noreply.github.com>
2022-06-26 00:17:27 +0900

    feat: 술 좋아요, 취소 api 구현  (#146)
    * feat: 술 좋아요, 싫어요 메소드 예외처리
    
    * feat: 술 좋아요 api 구현
    
    * feat: 좋아요 취소 api 구현
    
    * feat: 미로그인 상태로 술 api 사용 시 401 상태코드 반환
    
    * refactor: import 최적화

---
+++ pyeonsool/src/main/java/toyproject/pyeonsool/web/AlcoholApiController.java
@@ -0,0 +1,46 @@
+package toyproject.pyeonsool.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import toyproject.pyeonsool.LoginMember;
+import toyproject.pyeonsool.SessionConst;
+import toyproject.pyeonsool.service.AlcoholService;
+
+import static java.util.Objects.isNull;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/alcohols")
+public class AlcoholApiController {
+    private final AlcoholService alcoholService;
+
+    @PostMapping("/{alcoholId}/like")
+    public ResponseEntity<Void> likeAlcohol(
+            @PathVariable Long alcoholId,
+            @SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) LoginMember loginMember) {
+
+        if (isNull(loginMember)) {
+            // TODO advice 클래스 생성 후 예외 통합 관리
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        alcoholService.likeAlcohol(alcoholId, loginMember.getId());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{alcoholId}/dislike")
+    public ResponseEntity<Void> dislikeAlcohol(
+            @PathVariable Long alcoholId,
+            @SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) LoginMember loginMember) {
+
+        if (isNull(loginMember)) {
+            // TODO advice 클래스 생성 후 예외 통합 관리
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        alcoholService.dislikeAlcohol(alcoholId, loginMember.getId());
+
+        return ResponseEntity.ok().build();
+    }
+}
