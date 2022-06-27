41f9b223f829a48fb0e2aca77a36ae2a3a97c9fa
JunYoung-C <doforme524@gmail.com>
2022-06-27 17:37:35 +0900

    fix: 누락된 필드 추가

--- pyeonsool/src/main/java/toyproject/pyeonsool/service/ReviewDto.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/service/ReviewDto.java
@@ -2,6 +2,7 @@ package toyproject.pyeonsool.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 import toyproject.pyeonsool.domain.Review;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class ReviewDto {
+    private Long reviewId;
     private Long writerId;
     private String nickname;
     private Byte grade;
@@ -19,7 +21,8 @@ public class ReviewDto {
 
     public static ReviewDto from(Review review) {
         return new ReviewDto(review.getId(),
-                review.getContent(),
+                review.getMember().getId(),
+                review.getMember().getNickname(),
                 review.getGrade(),
                 review.getContent(),
                 review.getLastModifiedDate(),
