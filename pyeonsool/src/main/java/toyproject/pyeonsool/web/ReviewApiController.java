a8845e611796d0e07fe26db3c5094bf234c30027
JunYoung-C <doforme524@gmail.com>
2022-06-28 00:07:19 +0900

    feat: 리뷰 비추천 api 구현

---
+++ pyeonsool/src/main/java/toyproject/pyeonsool/web/ReviewApiController.java
@@ -0,0 +1,49 @@
+package toyproject.pyeonsool.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import toyproject.pyeonsool.LoginMember;
+import toyproject.pyeonsool.SessionConst;
+import toyproject.pyeonsool.domain.RecommendStatus;
+import toyproject.pyeonsool.service.ReviewService;
+
+import static java.util.Objects.isNull;
+
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor
+public class ReviewApiController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/{reviewId}/recommend")
+    public ResponseEntity<Void> recommendReview(
+            @PathVariable Long reviewId,
+            @SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) LoginMember loginMember) {
+
+        if (isNull(loginMember)) {
+            // TODO advice 클래스 생성 후 예외 통합 관리
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        reviewService.recommendReview(loginMember.getId(), reviewId, RecommendStatus.LIKE);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{reviewId}/not-recommend")
+    public ResponseEntity<Void> notRecommendReview(
+            @PathVariable Long reviewId,
+            @SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) LoginMember loginMember) {
+
+        if (isNull(loginMember)) {
+            // TODO advice 클래스 생성 후 예외 통합 관리
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        reviewService.recommendReview(loginMember.getId(), reviewId, RecommendStatus.DISLIKE);
+
+        return ResponseEntity.ok().build();
+    }
+}
