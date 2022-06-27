01dc117e9190f08b4574ec63fa95bef10ab62613
JunYoung-C <doforme524@gmail.com>
2022-06-27 23:42:44 +0900

    test: 좋아요 리스트 목록 조회 메소드 테스트

--- pyeonsool/src/test/java/toyproject/pyeonsool/service/MemberServiceTest.java
+++ pyeonsool/src/test/java/toyproject/pyeonsool/service/MemberServiceTest.java
@@ -6,10 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import toyproject.pyeonsool.LoginMember;
+import toyproject.pyeonsool.domain.Alcohol;
+import toyproject.pyeonsool.domain.AlcoholType;
 import toyproject.pyeonsool.domain.Member;
+import toyproject.pyeonsool.domain.PreferredAlcohol;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -66,4 +71,33 @@ class MemberServiceTest {
                     "잘못된 비밀번호입니다.");
         }
     }
+
+    @Test
+    void findLikeList() {
+        //given
+        Member member = new Member("nickname", "userId", "password", "01012345678");
+        em.persist(member);
+
+        Alcohol alcohol = new Alcohol(AlcoholType.WINE, "test.jpg", "옐로우테일", 35000, 13.5f,
+                (byte) 3, (byte) 2, "우리집", "대한민국");
+        Alcohol alcohol2 = new Alcohol(AlcoholType.SOJU, "test.jpg", "옐로우테일2", 35000, 13.5f,
+                (byte) 3, (byte) 2, "우리집", "대한민국");
+        Alcohol alcohol3 = new Alcohol(AlcoholType.WINE, "test.jpg", "옐로우테일3", 35000, 13.5f,
+                (byte) 3, (byte) 2, "우리집", "대한민국");
+        em.persist(alcohol);
+        em.persist(alcohol2);
+        em.persist(alcohol3);
+
+        em.persist(new PreferredAlcohol(member, alcohol));
+        em.persist(new PreferredAlcohol(member, alcohol2));
+        em.persist(new PreferredAlcohol(member, alcohol3));
+
+        //when
+        List<MyPageDto> likeList = memberService.findLikeList(member);
+
+        //then
+        assertThat(likeList.size()).isEqualTo(3);
+    }
+
+
 }
