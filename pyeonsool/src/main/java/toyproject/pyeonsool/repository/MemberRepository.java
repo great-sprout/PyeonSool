1f30213bca14e926c633aaa02ad8ea7a900250dd
wyu950 <dnjsdusdnr@naver.com>
2022-06-28 20:13:08 +0900

    feat: 내 Like 리스트 목록 조회 repository 구현 (#152)

--- pyeonsool/src/main/java/toyproject/pyeonsool/repository/MemberRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository  extends JpaRepository<Member, Long> {
 
     public Optional<Member> findByUserId(String userId);
+    Member findByNickname(String nickname);
 }
+
--- pyeonsool/src/main/java/toyproject/pyeonsool/repository/PreferredAlcoholCustomRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/repository/PreferredAlcoholCustomRepository.java
@@ -5,5 +5,4 @@ import java.util.List;
 public interface PreferredAlcoholCustomRepository {
    //내 Like 리스트 반환값
     List<Long> getMyList(Long memberId);
-    
 }
