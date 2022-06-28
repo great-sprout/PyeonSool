ac590ee6e8db46375cb49cd1a9ccb8d570bd23fa
jangsubin <toktoksbtn@gmail.com>
2022-06-28 16:33:11 +0900

    목록 페이지 주류 타이틀 변경 & 상품 목록 타임리프 적용 (#175,#184)
    * feat: 상품 목록 타임리프 적용
    
    * fix : 상품 목록 이름이 긴 제품을 위해 크기 수정
    
    * fix : 곰표 맥주 이미지 이름 수정
    
    * feat: URL 파라미터 값에 따라서 타이틀 이름 변경(소주 페이지 오류)

--- pyeonsool/src/main/resources/static/js/listPage.js
+++ pyeonsool/src/main/resources/static/js/listPage.js
@@ -15,4 +15,25 @@ function addBestSlidingEvent(){
         bestSlideController.slide(event.target, 4);
     });
 }
+function findType(){
+    const urlParams = new URL(location.href).searchParams;
+
+    const type = urlParams.get('alcoholType');
+
+    console.log(type);
+    let typeTxt;
+    if(type=="SOJU"){
+        typeTxt = "소주";
+    }
+     else if(type=="BEER"){
+        typeTxt = "맥주";
+    }
+    else if(type=="WINE"){
+         typeTxt = "와인";
+    }
+    console.log("typetxt = "+typeTxt);
+    document.getElementById('alcoholTitle').innerHTML=typeTxt;
+
+}
+ findType();
 addBestSlidingEvent();
