1c7c18c2f58e3a59a9638e1f3609d44fd44b45c5
wyu950 <dnjsdusdnr@naver.com>
2022-06-28 20:15:03 +0900

    feat: 내 Like 리스트 목록 DTO  및 컨트롤러 구현 (#152)

--- pyeonsool/src/main/java/toyproject/pyeonsool/service/MyPageDto.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/service/MyPageDto.java
@@ -7,28 +7,19 @@ import lombok.ToString;
 import toyproject.pyeonsool.domain.Member;
 import toyproject.pyeonsool.domain.PreferredAlcohol;
 
+import java.util.List;
+
 @Data
 public class MyPageDto {
+    //마이페이지 dto이므로 이에 해당하는 멤버의 아이디가 들어가야한다.
+    //멤버가 좋아요누른 목록도 있어야 하므로 좋아요누른 술들의 아이디와 이미지 저장경로 작성
     private Long memberId;
-    private Long alcoholId;
-    private Long preferredAlcoholId;
-    private String imagePath;
+    private List<Long> alcoholIds;
+    private List<String> imagePaths;
 
-
-    //값만띄워서 값전달
-
-    public MyPageDto(Long memberId, Long alcoholId, Long preferredAlcoholId, String imagePath) {
+    public MyPageDto(Long memberId, List<Long> alcoholIds, List<String> imagePaths) {
         this.memberId = memberId;
-        this.alcoholId = alcoholId;
-        this.preferredAlcoholId = preferredAlcoholId;
-        this.imagePath = imagePath;
-
-    }
-
-    public MyPageDto(PreferredAlcohol pa, String imagePath) {
-        this(pa.getMember().getId(),
-                pa.getAlcohol().getId(),
-                pa.getId(),
-                imagePath);
+        this.alcoholIds = alcoholIds;
+        this.imagePaths = imagePaths;
     }
 }
