b61471a71ab4c57515c1b431054df80ae963641d
JunYoung-C <doforme524@gmail.com>
2022-06-28 13:36:44 +0900

    test: 리뷰 추천/비추천 개수 조회 메소드 테스트

--- pyeonsool/src/test/java/toyproject/pyeonsool/repository/RecommendedReviewRepositoryTest.java
+++ pyeonsool/src/test/java/toyproject/pyeonsool/repository/RecommendedReviewRepositoryTest.java
@@ -80,4 +80,41 @@ class RecommendedReviewRepositoryTest {
                 .isEmpty();
     }
 
+    @Test
+    void getRecommendCountGroupBy() {
+        //given
+        Member member =
+                new Member("준영이", "chlwnsdud121", "1234", "01012345678");
+        Member member2 =
+                new Member("영준이", "chldudwns121", "1234", "01023456789");
+        Member member3 =
+                new Member("춘향이", "chlcnsgid121", "1234", "01043218765");
+
+        em.persist(member);
+        em.persist(member2);
+        em.persist(member3);
+
+        Alcohol alcohol = new Alcohol(BEER, "san-miguel.png", "산미구엘 페일필젠", 3000,
+                5f, null, null, "산미구엘 브루어리", "필리핀");
+        em.persist(alcohol);
+
+        Review review = new Review(member, alcohol, (byte) (3), "테스트 리뷰");
+        em.persist(review);
+
+        RecommendedReview[] recommendedReviews = new RecommendedReview[3];
+        recommendedReviews[0] = new RecommendedReview(member, review, RecommendStatus.LIKE);
+        recommendedReviews[1] = new RecommendedReview(member2, review, RecommendStatus.DISLIKE);
+        recommendedReviews[2] = new RecommendedReview(member3, review, RecommendStatus.LIKE);
+        for (RecommendedReview recommendedReview : recommendedReviews) {
+            em.persist(recommendedReview);
+        }
+        //when
+        Long likeCount = recommendedReviewRepository.getRecommendCountGroupBy(review.getId(), RecommendStatus.LIKE);
+        Long dislikeCount = recommendedReviewRepository.getRecommendCountGroupBy(review.getId(), RecommendStatus.DISLIKE);
+
+        //then
+        assertThat(likeCount).isEqualTo(2);
+        assertThat(dislikeCount).isEqualTo(1);
+    }
+
 }
