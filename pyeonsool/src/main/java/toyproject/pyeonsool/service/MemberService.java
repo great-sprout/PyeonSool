9770929163ca596b9ace3b92d8162d615a869e41
JunYoung-C <doforme524@gmail.com>
2022-06-30 20:48:27 +0900

    feat: 회원가입 로직에 내 키워드 저장 기능 추가

--- pyeonsool/src/main/java/toyproject/pyeonsool/service/MemberService.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/service/MemberService.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import toyproject.pyeonsool.FileManager;
 import toyproject.pyeonsool.LoginMember;
+import toyproject.pyeonsool.domain.Keyword;
 import toyproject.pyeonsool.domain.Member;
+import toyproject.pyeonsool.domain.MyKeyword;
 import toyproject.pyeonsool.domain.PreferredAlcohol;
-import toyproject.pyeonsool.repository.AlcoholRepository;
-import toyproject.pyeonsool.repository.MemberRepository;
-import toyproject.pyeonsool.repository.PreferredAlcoholRepository;
+import toyproject.pyeonsool.repository.*;
 import toyproject.pyeonsool.web.MemberSaveForm;
 
 import javax.transaction.Transactional;
@@ -21,6 +21,8 @@ import java.util.stream.Collectors;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final KeywordRepository keywordRepository;
+    private final MyKeywordRepository myKeywordRepository;
 
     public LoginMember findLoginMember(String userId, String password) {
         Member findMember = memberRepository.findByUserId(userId)
@@ -35,7 +37,19 @@ public class MemberService {
         return new LoginMember(findMember.getId(), findMember.getNickname());
     }
 
-    public Long signup(String nickname, String userId, String password, String phoneNumber, List<Long> keywords) {
+    public Long signup(String nickname, String userId, String password, String phoneNumber, List<String> keywordNames) {
+        validateSignup(nickname, userId, phoneNumber);
+
+        Member member = new Member(nickname, userId, password, phoneNumber);
+        memberRepository.save(member);
+
+        keywordRepository.findKeywordsByNameIn(keywordNames)
+                .forEach(keyword -> myKeywordRepository.save(new MyKeyword(member, keyword)));
+
+        return member.getId();
+    }
+
+    private void validateSignup(String nickname, String userId, String phoneNumber) {
         if (memberRepository.existsByNickname(nickname)) {
             throw new IllegalArgumentException("이미 사용중인 닉네임입니다.");
         }
@@ -47,9 +61,5 @@ public class MemberService {
         if (memberRepository.existsByPhoneNumber(phoneNumber)) {
             throw new IllegalArgumentException("이미 사용중인 핸드폰 번호입니다.");
         }
-
-        Member member = new Member(nickname, userId, password, phoneNumber);
-        memberRepository.save(member);
-        return member.getId();
     }
 }
