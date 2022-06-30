712bfaef3c1881354af7e01985f945596ea9c5f5
JunYoung-C <doforme524@gmail.com>
2022-06-30 20:38:31 +0900

    feat: 키워드 이름으로 키워드 객체 조회하는 메소드 구현

--- pyeonsool/src/main/java/toyproject/pyeonsool/repository/KeywordRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/repository/KeywordRepository.java
@@ -3,5 +3,8 @@ package toyproject.pyeonsool.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import toyproject.pyeonsool.domain.Keyword;
 
+import java.util.Optional;
+
 public interface KeywordRepository  extends JpaRepository<Keyword, Long> {
+    Optional<Keyword> findByName(String name);
 }
