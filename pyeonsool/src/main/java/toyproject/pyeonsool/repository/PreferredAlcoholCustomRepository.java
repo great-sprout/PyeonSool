49e3050dfd23b8d5ab7a70a8269696e1cd6112f9
wyu950 <dnjsdusdnr@naver.com>
2022-06-30 16:32:51 +0900

    fix: 마이 페이지 내 Like 리스트 repository수정

--- pyeonsool/src/main/java/toyproject/pyeonsool/repository/PreferredAlcoholCustomRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/repository/PreferredAlcoholCustomRepository.java
@@ -1,19 +1,19 @@
 package toyproject.pyeonsool.repository;
 
+import toyproject.pyeonsool.domain.Alcohol;
 import toyproject.pyeonsool.domain.AlcoholType;
 
 import java.util.List;
 
 public interface PreferredAlcoholCustomRepository {
-    //내 Like 리스트 반환값
-    List<Long> getMyList(Long memberId);
-
     List<Long> getAlcoholIds();
 
     List<Long> getAlcoholByType(AlcoholType type);
 
     Long getLikeCount(Long alcoholId);
 
+    //내 Like 리스트 반환값
+    List<Alcohol> getAlcohols(Long memberId, Long limit);
 
 
 }
