740a53c086024b8a6a6d30f2a5a27364e24cae90
JunYoung Choi <87891581+JunYoung-C@users.noreply.github.com>
2022-07-03 13:40:28 +0900

    fix: 로그인 회원 이외의 닉네임으로 마이페이지 접속되는 현상 (#255)
    * feat: nickname에 해당되는 memberId를 반환하는 기능 구현
    
    * feat:에러 페이지 생성
    
    * feat: 마이페이지 잘못된 닉네임으로 접근 시 에러 페이지로 이동

--- pyeonsool/src/main/java/toyproject/pyeonsool/repository/MemberRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface MemberRepository  extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUserId(String userId);
-    Member findByNickname(String nickname);
+    Optional<Member> findByNickname(String nickname);
 
     boolean existsByNickname(String nickname);
     boolean existsByUserId(String userId);
