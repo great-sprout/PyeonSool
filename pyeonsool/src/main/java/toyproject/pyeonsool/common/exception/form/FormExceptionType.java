b9aa6f114f5a1567571daa64f5eb6824b8bb2e04
JunYoung-C <doforme524@gmail.com>
2022-07-09 08:34:15 +0900

    fix: 예외 클래스 수정

--- pyeonsool/src/main/java/toyproject/pyeonsool/common/exception/form/FormExceptionType.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/common/exception/form/FormExceptionType.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum FormExceptionType {
     INCORRECT_PASSWORD(new IncorrectPasswordException("잘못된 비밀번호입니다.", "incorrect", "password")),
-    INCORRECT_USER_ID(new IncorrectPasswordException(
+    INCORRECT_USER_ID(new IncorrectUserIdException(
             "입력한 아이디를 사용하는 계정을 찾을 수 없습니다.", "incorrect", "userId"));
 
     private final PyeonSoolFormException exception;
