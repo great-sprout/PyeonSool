0fbbd86b2c06c00813e31d340103fa2066ead7dd
JunYoung-C <doforme524@gmail.com>
2022-07-08 13:34:55 +0900

    feat: 로그인 페이지 필드 에외 관련 클래스 생성

---
+++ pyeonsool/src/main/java/toyproject/pyeonsool/common/exception/form/FormExceptionType.java
@@ -0,0 +1,14 @@
+package toyproject.pyeonsool.common.exception.form;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum FormExceptionType {
+    INCORRECT_PASSWORD(new IncorrectPasswordException("잘못된 비밀번호입니다.", "incorrect", "password")),
+    INCORRECT_USER_ID(new IncorrectPasswordException(
+            "입력한 아이디를 사용하는 계정을 찾을 수 없습니다.", "incorrect", "userId"));
+
+    private final PyeonSoolFormException exception;
+}
