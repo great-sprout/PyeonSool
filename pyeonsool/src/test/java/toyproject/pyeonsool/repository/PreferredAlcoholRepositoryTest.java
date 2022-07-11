09b84ab3871c48ea85698d5063382a1733cdbc2f
JunYoung-C <doforme524@gmail.com>
2022-07-11 23:03:12 +0900

    test: likeAlcohol, dislikeAlcohol 관련 메소드 테스트 코드 작성

--- pyeonsool/src/test/java/toyproject/pyeonsool/repository/PreferredAlcoholRepositoryTest.java
+++ pyeonsool/src/test/java/toyproject/pyeonsool/repository/PreferredAlcoholRepositoryTest.java
@@ -7,11 +7,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import toyproject.pyeonsool.AppConfig;
 import toyproject.pyeonsool.domain.Alcohol;
+import toyproject.pyeonsool.domain.AlcoholType;
 import toyproject.pyeonsool.domain.Member;
 import toyproject.pyeonsool.domain.PreferredAlcohol;
 import toyproject.pyeonsool.preferredalcohol.repository.PreferredAlcoholRepository;
 
 import javax.persistence.EntityManager;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @Import(AppConfig.class)
@@ -26,29 +31,47 @@ class PreferredAlcoholRepositoryTest {
     @Test
     void should_Exist_When_PreferredAlcoholIsObtained() {
         //given
-        Member member = new Member(null, null, null, null);
+        Member member = new Member("nickname", "userId", "password", "01012345678");
         em.persist(member);
 
-        Alcohol alcohol = new Alcohol(null, null, null, null, null, null,
-                null, null, null);
+        Alcohol alcohol = new Alcohol(AlcoholType.WINE, "test.jpg", "옐로우테일", 35000, 13.5f,
+                (byte) 3, (byte) 2, "우리집", "대한민국");
         em.persist(alcohol);
 
-        //when
         em.persist(new PreferredAlcohol(member, alcohol));
 
+        //when
         //then
-        Assertions.assertThat(preferredAlcoholRepository.existsByMemberAndAlcohol(member, alcohol))
+        assertThat(preferredAlcoholRepository.existsByMemberAndAlcohol(member, alcohol))
                 .isTrue();
     }
 
     @Test
-    void removeByMemberAndAlcohol() {
+    void should_Success_When_CountingPreferredAlcohols() {
         //given
-        Member member = new Member(null, null, null, null);
+        Member member = new Member("nickname", "userId", "password", "01012345678");
         em.persist(member);
 
-        Alcohol alcohol = new Alcohol(null, null, null, null, null, null,
-                null, null, null);
+        Alcohol alcohol = new Alcohol(AlcoholType.WINE, "test.jpg", "옐로우테일", 35000, 13.5f,
+                (byte) 3, (byte) 2, "우리집", "대한민국");
+        em.persist(alcohol);
+
+        em.persist(new PreferredAlcohol(member, alcohol));
+
+        //when
+        //then
+        assertThat(preferredAlcoholRepository.countByMemberId(member.getId())).isEqualTo(1);
+    }
+
+
+    @Test
+    void should_Success_When_PreferredAlcoholIsRemoved() {
+        //given
+        Member member = new Member("nickname", "userId", "password", "01012345678");
+        em.persist(member);
+
+        Alcohol alcohol = new Alcohol(AlcoholType.WINE, "test.jpg", "옐로우테일", 35000, 13.5f,
+                (byte) 3, (byte) 2, "우리집", "대한민국");
         em.persist(alcohol);
 
         PreferredAlcohol preferredAlcohol = new PreferredAlcohol(member, alcohol);
@@ -58,6 +81,6 @@ class PreferredAlcoholRepositoryTest {
         preferredAlcoholRepository.removeByMemberAndAlcohol(member, alcohol);
 
         //then
-        Assertions.assertThat(preferredAlcoholRepository.findById(preferredAlcohol.getId())).isEmpty();
+        assertThat(preferredAlcoholRepository.findById(preferredAlcohol.getId())).isEmpty();
     }
 }
