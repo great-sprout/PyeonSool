a407ba307b65aec454aa6c7ed48e4e6d557b7ab3
JunYoung-C <doforme524@gmail.com>
2022-07-14 17:20:04 +0900

    style: 공백 제거

--- pyeonsool/src/main/java/toyproject/pyeonsool/keyword/repository/KeywordRepository.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/keyword/repository/KeywordRepository.java
@@ -6,6 +6,6 @@ import toyproject.pyeonsool.domain.Keyword;
 import java.util.List;
 import java.util.Optional;
 
-public interface KeywordRepository  extends JpaRepository<Keyword, Long> {
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     List<Keyword> findKeywordsByNameIn(List<String> names);
 }
