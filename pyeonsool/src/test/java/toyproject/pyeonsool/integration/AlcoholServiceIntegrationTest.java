4c0831d483c6c93ebcae3ed3c24ab3c9eeac4eac
JunYoung-C <doforme524@gmail.com>
2022-07-16 10:40:49 +0900

    test: 통합 테스트 폴더 명 변경

--- pyeonsool/src/test/java/toyproject/pyeonsool/integration/AlcoholServiceIntegrationTest.java
+++ pyeonsool/src/test/java/toyproject/pyeonsool/integration/AlcoholServiceIntegrationTest.java
@@ -1,4 +1,4 @@
-package toyproject.pyeonsool.service;
+package toyproject.pyeonsool.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- pyeonsool/src/test/java/toyproject/pyeonsool/integration/MemberServiceIntegrationTest.java
+++ pyeonsool/src/test/java/toyproject/pyeonsool/integration/MemberServiceIntegrationTest.java
@@ -1,4 +1,4 @@
-package toyproject.pyeonsool.service;
+package toyproject.pyeonsool.integration;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
