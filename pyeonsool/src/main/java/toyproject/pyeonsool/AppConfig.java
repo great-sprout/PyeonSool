8c34e9d91a393b9a4ad093ebc976963ac913ff07
JunYoung-C <doforme524@gmail.com>
2022-12-04 16:18:40 +0900

    feat: api 요청 시 로그인 검증 조건 추가

--- pyeonsool/src/main/java/toyproject/pyeonsool/AppConfig.java
+++ pyeonsool/src/main/java/toyproject/pyeonsool/AppConfig.java
@@ -18,7 +18,7 @@ public class AppConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/members/login", "/members/add", "/members/logout",
-                        "/css/**", "/*.ico", "/js/**", "/image/**", "/alcohols/**", "/alcohols", "/reviews/**",
+                        "/css/**", "/*.ico", "/js/**", "/image/**", "/alcohols",
                         "/error");
     }
 }
